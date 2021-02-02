3ff003e4d11a6c0a51b576bf1f2356eb13d9ebe1
MarvinMichel <mmhsernee@gmail.com>
2021-02-02 17:58:08 +0100

    Add styling and first fetch functions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ImageFeed
+# ImageFeeder
 
-ImageFeed is an application build with the [Unsplash API](https://github.com/unsplash/unsplash-js). It shows you a feed of the latest photo's added to Unsplash. You can view the photo's and their details, search for specific photo's trough keywords or just view random pictures.
+ImageFeeder is an application build with the [Unsplash API](https://github.com/unsplash/unsplash-js). It shows you a feed of the latest photo's added to Unsplash. You can view the photo's and their details, search for specific photo's trough keywords or just view random pictures.
 
 ## Unsplash API
 We use the Unsplash API to get the photo's and search trough their database with keywords. For more information to use the API, check the [documentation](https://unsplash.com/documentation).
--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-window.addEventListener('load', () => {
-  console.log('I\'m connected!')
-})
+(function () {
+  const apiKey = 'lbhvcWW7O-NY5XERiGF9BrvC2L05e-MWIjagBBsXr9A'
+
+  const searchForm = document.querySelector('form')
+  searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+    if (search.value) searchImages(search.value)
+  })
+
+  function getRandomImage() {
+    const apiURL = 'https://api.unsplash.com/photos/random/'
+  
+    return fetch(`${apiURL}?client_id=${apiKey}`)
+      .then(res => res.json())
+      .then(data => console.log(data.urls.regular))
+      .catch(err => console.log('Something went wrong', err))
+  }
+  
+  function searchImages(query) {
+    const apiURL = 'https://api.unsplash.com/search/photos/'
+  
+    return fetch(`${apiURL}?query=${query}&client_id=${apiKey}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log('Something went wrong', err))
+  }
+})()
