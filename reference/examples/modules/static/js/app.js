ea356bb09d37c776273e4555749976c635926c46
MarvinMichel <mmhsernee@gmail.com>
2021-02-05 11:55:06 +0100

    Remove old files

--- reference/examples/modules/static/js/app.js
+++
@@ -1,9 +0,0 @@
-import { handleRoutes } from './modules/router.js';
-
-handleRoutes();
-
-/*** good to know ***/
-
-// modules are scoped
-// modules only work on webserver / localhost
-// main script needs [type=module] attribute to work
